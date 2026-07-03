a1b2a90b27a209fce2aa656ae677910ee0b28437
whonore <wolfhonore@gmail.com>
2026-07-03 10:30:45 -0700

    feat: basic rolling, min, max

---
+++ src/expr.rs
@@ -0,0 +1,74 @@
+use std::iter;
+
+#[derive(Clone, Debug)]
+pub enum Expr {
+    Die { sides: usize },
+    Repeat { expr: Box<Self>, n: usize },
+}
+
+impl Expr {
+    pub const fn die(sides: usize) -> Self {
+        Self::Die { sides }
+    }
+
+    pub fn repeat(self, n: usize) -> Self {
+        Self::Repeat {
+            expr: Box::new(self),
+            n,
+        }
+    }
+
+    pub fn roll(&self) -> usize {
+        match self {
+            Self::Die { sides: 0 } => 0,
+            Self::Die { sides } => rand::random_range(1..=*sides),
+            Self::Repeat { expr, n } => iter::repeat_with(|| expr.roll()).take(*n).sum(),
+        }
+    }
+
+    pub fn min(&self) -> usize {
+        match self {
+            Self::Die { sides: 0 } => 0,
+            Self::Die { .. } => 1,
+            Self::Repeat { expr, n } => expr.min() * n,
+        }
+    }
+
+    pub fn max(&self) -> usize {
+        match self {
+            Self::Die { sides } => *sides,
+            Self::Repeat { expr, n } => expr.max() * n,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use proptest::prelude::*;
+
+    use super::*;
+
+    fn arb_expr() -> impl Strategy<Value = Expr> {
+        const MAX_DEPTH: u32 = 4;
+        const DESIRED_SIZE: u32 = 64;
+        const EXPECTED_BRANCH_SIZE: u32 = 8;
+
+        let leaf = prop_oneof![(0usize..=256).prop_map(Expr::die)];
+        leaf.prop_recursive(MAX_DEPTH, DESIRED_SIZE, EXPECTED_BRANCH_SIZE, |inner| {
+            prop_oneof![inner.prop_flat_map(|expr| {
+                (0usize..=256).prop_map(move |n| Expr::repeat(expr.clone(), n))
+            })]
+        })
+    }
+
+    proptest! {
+        #[test]
+        fn roll_in_range(expr in arb_expr()) {
+            let v = expr.roll();
+            let min = expr.min();
+            let max = expr.max();
+            assert!(min <= v, "{min} <= {v}");
+            assert!(v <= max, "{v} <= {max}");
+        }
+    }
+}
